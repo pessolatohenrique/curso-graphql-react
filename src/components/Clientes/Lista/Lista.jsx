1da66608cb9b4da307d4cb3db3acf931c7f9d377
Henrique Pessolato <henrique.pessolato@ftd.comm.br>
2020-09-26 22:09:35 -0300

    Instalacao e configuracao do material-ui

--- src/components/Clientes/Lista/Lista.jsx
+++ src/components/Clientes/Lista/Lista.jsx
@@ -1,5 +1,18 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import Container from "@material-ui/core/Container";
+import Card from "@material-ui/core/Card";
+import { Create, Delete } from "@material-ui/icons";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 import clientesApi from "../../../api/clientes";
 import { Query } from "react-apollo";
 import { LISTA_CLIENTES } from "../../../graphql/clientes";
@@ -18,44 +31,64 @@ class Clientes extends React.Component {
   render() {
     return (
       <div>
-        <div className="header">
-          <h1>Clientes</h1>
-
-          <Link to="/clientes/novo">Novo Cliente </Link>
-        </div>
-
-        <table>
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>CPF</th>
-              <th>Ações</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            <Query query={LISTA_CLIENTES}>
-              {({ data }) => {
-                if (!data) return null;
-                return data.clientes.map((cliente) => (
-                  <tr key={cliente.cpf}>
-                    <td>{cliente.nome}</td>
-                    <td>{cliente.cpf}</td>
-                    <td>
-                      <Link to={`clientes/${cliente.id}`}>visualizar</Link>
-                      <Link to={`clientes/alterar/${cliente.id}`}>alterar</Link>
-                      <button
-                        onClick={this.deletarCliente.bind(this, cliente.id)}
-                      >
-                        remover
-                      </button>
-                    </td>
-                  </tr>
-                ));
-              }}
-            </Query>
-          </tbody>
-        </table>
+        <Container>
+          <Card>
+            <CardContent>
+              <Typography variant="h5" component="h2" color="primary">
+                Clientes
+              </Typography>
+              <br />
+              <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Nome</TableCell>
+                      <TableCell>CPF</TableCell>
+                      <TableCell>Ações</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    <Query query={LISTA_CLIENTES}>
+                      {({ data }) => {
+                        if (!data) return null;
+                        return data.clientes.map((cliente) => (
+                          <TableRow key={cliente.cpf}>
+                            <TableCell component="th" scope="row">
+                              {cliente.nome}
+                            </TableCell>
+                            <TableCell>{cliente.cpf}</TableCell>
+                            <TableCell>
+                              <Button
+                                color="primary"
+                                href={`clientes/alterar/${cliente.id}`}
+                              >
+                                <Create />
+                              </Button>
+                              <Button
+                                color="secondary"
+                                onClick={this.deletarCliente.bind(
+                                  this,
+                                  cliente.id
+                                )}
+                              >
+                                <Delete />
+                              </Button>
+                            </TableCell>
+                          </TableRow>
+                        ));
+                      }}
+                    </Query>
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </CardContent>
+            <CardActions>
+              <Button size="small" color="primary" href="/clientes/novo">
+                Adicionar Cliente
+              </Button>
+            </CardActions>
+          </Card>
+        </Container>
       </div>
     );
   }
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,28 +1,100 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './styles.scss'
+import React from "react";
+import Drawer from "@material-ui/core/Drawer";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import PeopleIcon from "@material-ui/icons/People";
+import PetsIcon from "@material-ui/icons/Pets";
+import MenuIcon from "@material-ui/icons/Menu";
+import RoomServiceIcon from "@material-ui/icons/RoomService";
+import ScheduleIcon from "@material-ui/icons/Schedule";
+import "./styles.scss";
 
-const Menu = () => (
-  <nav>
-    <ul className="menu">
-      <li className="menu__item">
-        <Link className="menu__link" to="/">Home</Link>
-      </li>
-      <li className="menu__item">
-        <Link  className="menu__link" to="/clientes">Clientes</Link>
-      </li>
-      <li className="menu__item">
-        <Link  className="menu__link" to="/pets">Pets</Link>
-      </li>
-      <li className="menu__item">
-        <Link  className="menu__link" to="/servicos">Serviços</Link>
-      </li>
-      <li className="menu__item">
-        <Link  className="menu__link" to="/atendimentos">Atendimentos
-        </Link>
-      </li>
-    </ul>
-  </nav>
-)
+const items = [
+  {
+    table: "Dashboard",
+    icon: <DashboardIcon />,
+  },
+  {
+    table: "atendimentos",
+    icon: <ScheduleIcon />,
+  },
+  {
+    table: "clientes",
+    icon: <PeopleIcon />,
+  },
+  {
+    table: "pets",
+    icon: <PetsIcon />,
+  },
+  {
+    table: "servicos",
+    icon: <RoomServiceIcon />,
+  },
+];
 
-export default Menu
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleDrawerOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleDrawerClose = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { open } = this.state;
+
+    return (
+      <nav>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              onClick={this.handleDrawerOpen}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">Agenda Petshop</Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="persistent" anchor="left" open={open}>
+          <div>
+            <IconButton onClick={this.handleDrawerClose}>
+              <ChevronLeftIcon />
+            </IconButton>
+            <List>
+              {items.map((item, key) => (
+                <ListItem button key={key}>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <Button size="small" color="primary" href={`/${item.table}`}>
+                    {item.table}
+                  </Button>
+                </ListItem>
+              ))}
+            </List>
+          </div>
+        </Drawer>
+        <br />
+      </nav>
+    );
+  }
+}
+
+export default Menu;
