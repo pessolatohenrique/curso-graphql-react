91a8baa2d85ce89f3fba4d8faf06d1bcb0b403f8
Henrique Pessolato <pessolatohenrique@gmail.com>
2020-09-27 14:31:56 -0300

    Construcao de formulario com Formik

--- src/components/Clientes/Novo/Novo.jsx
+++ src/components/Clientes/Novo/Novo.jsx
@@ -1,53 +1,98 @@
-import React from 'react'
-import clientesApi from '../../../api/clientes'
-import clientes from '../../../api/clientes';
+import React from "react";
+import {
+  REQUIRED_MESSAGE,
+  TOO_SHORT_MESSAGE,
+  TOO_LONG_MESSAGE,
+} from "../../../constants";
+import clientesApi from "../../../api/clientes";
+import { withFormik, Form } from "formik";
+import * as Yup from "yup";
+import {
+  Typography,
+  Container,
+  Card,
+  CardContent,
+  TextField,
+  Button,
+} from "@material-ui/core";
 
-class Novo extends React.Component {
-  constructor(props) {
-    super(props)
+const ValidationSchema = Yup.object().shape({
+  nome: Yup.string()
+    .min(2, TOO_SHORT_MESSAGE)
+    .max(50, TOO_LONG_MESSAGE)
+    .required(REQUIRED_MESSAGE),
+  cpf: Yup.string().required(REQUIRED_MESSAGE),
+});
 
-    this.state = {
-      nome: '',
-      cpf: ''
-    }
+const FormComponent = (props) => {
+  const { values, isSubmitting, handleChange, errors, touched } = props;
+  const { nome, cpf } = values;
 
-    this.gerenciarMudancas = this.gerenciarMudancas.bind(this)
-    this.gerenciarEnvio = this.gerenciarEnvio.bind(this)
-  }
+  return (
+    <Container>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="h2" color="primary">
+            Novo Cliente
+          </Typography>
+          <br />
+          <Form>
+            <TextField
+              name="nome"
+              id="nome"
+              placeholder="Preencha aqui"
+              label="Nome"
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+              error={errors.nome && touched.nome}
+              helperText={
+                errors.nome && touched.nome ? <div>{errors.nome}</div> : null
+              }
+              onChange={handleChange}
+              value={nome}
+            />
+            <br />
+            <br />
+            <TextField
+              name="cpf"
+              id="cpf"
+              placeholder="Preencha aqui"
+              label="CPF"
+              fullWidth
+              InputLabelProps={{
+                shrink: true,
+              }}
+              error={errors.cpf && touched.cpf}
+              helperText={
+                errors.cpf && touched.cpf ? <div>{errors.cpf}</div> : null
+              }
+              onChange={handleChange}
+              value={cpf}
+            />
+            <br />
+            <br />
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              disabled={isSubmitting}
+            >
+              Adicionar
+            </Button>
+          </Form>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
 
-  gerenciarMudancas(evento) {
-    const chave = evento.target.name
-    const valor = evento.target.value
-    this.setState({ [chave]: valor })
-  }
-
-  gerenciarEnvio(evento) {
-    evento.preventDefault()
-    
-    clientesApi.adicionarCliente(this.state)
-    this.props.history.push('/clientes')
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Novo Cliente</h1>
-
-        <form onSubmit={this.gerenciarEnvio}>
-          <div>
-            <label htmlFor="nome">Nome</label>
-            <input type="text" name="nome" id="nome" value={this.state.nome} onChange={this.gerenciarMudancas} />
-          </div>
-          <div>
-            <label htmlFor="cpf">CPF</label>
-            <input type="text" name="cpf" id="cpf" value={this.state.cpf} onChange={this.gerenciarMudancas} />
-          </div>
-
-          <button type="submit">Enviar</button>
-        </form>
-      </div>
-    )
-  }
-}
-
-export default Novo
+export default withFormik({
+  mapPropsToValues: () => ({ nome: "", cpf: "" }),
+  validationSchema: ValidationSchema,
+  handleSubmit: (values, actions) => {
+    clientesApi.adicionarCliente(values);
+    actions.props.history.push("/clientes");
+  },
+})(FormComponent);
---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const REQUIRED_MESSAGE = "Campo de preenchimento obrigatório";
+export const TOO_SHORT_MESSAGE = "Texto muito curto!";
+export const TOO_LONG_MESSAGE = "Texto muito longo!";
