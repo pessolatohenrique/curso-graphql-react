be25d207544e4aec15d55900ec7a1103e2a28b07
pessolatohenrique <pessolatohenrique@gmail.com>
2020-10-24 18:39:19 -0300

    Atualizacao de cliente

--- src/components/Clientes/Alterar/Alterar.jsx
+++ src/components/Clientes/Alterar/Alterar.jsx
@@ -1,59 +1,136 @@
-import React from 'react'
-import clientesApi from '../../../api/clientes'
+import React from "react";
+import clientesApi from "../../../api/clientes";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import {
+  REQUIRED_MESSAGE,
+  TOO_SHORT_MESSAGE,
+  TOO_LONG_MESSAGE,
+} from "../../../constants";
+import {
+  Typography,
+  Container,
+  Card,
+  CardContent,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { CpfMaskCustom, unmask } from "../../../helpers/MaskInput";
+import { Query, Mutation } from "react-apollo";
+import { CONSULTA_CLIENTE, ATUALIZA_CLIENTE } from "../../../graphql/clientes";
 
-class Alterar extends React.Component {
-  constructor(props) {
-    super(props)
+const ValidationSchema = Yup.object().shape({
+  nome: Yup.string()
+    .min(2, TOO_SHORT_MESSAGE)
+    .max(50, TOO_LONG_MESSAGE)
+    .required(REQUIRED_MESSAGE),
+  cpf: Yup.string().required(REQUIRED_MESSAGE),
+});
 
-    this.state = {
-      nome: '',
-      cpf: ''
-    }
+const FormComponent = (props) => {
+  return (
+    <Container>
+      <Card>
+        <CardContent>
+          <Typography variant="h5" component="h2" color="primary">
+            Atualizar Cliente
+          </Typography>
+          <br />
+          <Query
+            query={CONSULTA_CLIENTE}
+            variables={{ id: parseInt(props.match.params.id) }}
+          >
+            {({ data, loading }) => {
+              if (loading) return null;
 
-    this.gerenciarMudancas = this.gerenciarMudancas.bind(this)
-    this.gerenciarEnvio = this.gerenciarEnvio.bind(this)
-  }
+              const { cliente } = data;
+              const { nome, cpf } = cliente;
 
-  componentDidMount() {
-    const { id } = this.props.match.params
-    clientesApi.buscarClientePorId(id)
-      .then(cliente => this.setState({ nome: cliente.nome, cpf: cliente.cpf }))
-  }
+              return (
+                <Mutation mutation={ATUALIZA_CLIENTE}>
+                  {(atualizaCliente, data) => {
+                    return (
+                      <Formik
+                        initialValues={{ nome, cpf }}
+                        validationSchema={ValidationSchema}
+                        onSubmit={(values, actions) => {
+                          const { id } = props.match.params;
+                          values.cpf = unmask(values.cpf);
+                          atualizaCliente({ variables: { id, ...values } });
+                          props.history.push("/clientes");
+                        }}
+                        render={({
+                          errors,
+                          touched,
+                          values,
+                          isSubmitting,
+                          handleChange,
+                        }) => (
+                          <Form>
+                            <TextField
+                              name="nome"
+                              id="nome"
+                              placeholder="Preencha aqui"
+                              label="Nome"
+                              fullWidth
+                              InputLabelProps={{
+                                shrink: true,
+                              }}
+                              error={errors.nome && touched.nome}
+                              helperText={
+                                errors.nome && touched.nome ? (
+                                  <div>{errors.nome}</div>
+                                ) : null
+                              }
+                              onChange={handleChange}
+                              value={values.nome}
+                            />
+                            <br />
+                            <br />
+                            <TextField
+                              name="cpf"
+                              id="cpf"
+                              placeholder="Preencha aqui"
+                              label="CPF"
+                              fullWidth
+                              InputLabelProps={{
+                                shrink: true,
+                              }}
+                              InputProps={{
+                                inputComponent: CpfMaskCustom,
+                              }}
+                              error={errors.cpf && touched.cpf}
+                              helperText={
+                                errors.cpf && touched.cpf ? (
+                                  <div>{errors.cpf}</div>
+                                ) : null
+                              }
+                              onChange={handleChange}
+                              value={values.cpf}
+                            />
+                            <br />
+                            <br />
+                            <Button
+                              variant="contained"
+                              color="primary"
+                              type="submit"
+                              disabled={isSubmitting}
+                            >
+                              Atualizar
+                            </Button>
+                          </Form>
+                        )}
+                      ></Formik>
+                    );
+                  }}
+                </Mutation>
+              );
+            }}
+          </Query>
+        </CardContent>
+      </Card>
+    </Container>
+  );
+};
 
-  gerenciarMudancas(evento) {
-    const chave = evento.target.name
-    const valor = evento.target.value
-    this.setState({ [chave]: valor })
-  }
-
-  gerenciarEnvio(evento) {
-    evento.preventDefault()
-    const { id } = this.props.match.params
-
-    clientesApi.alterarCliente(id, this.state)
-    this.props.history.push('/clientes')
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Alterar Cliente</h1>
-
-        <form onSubmit={this.gerenciarEnvio}>
-          <div>
-            <label htmlFor="nome">Nome</label>
-            <input type="text" name="nome" id="nome" value={this.state.nome} onChange={this.gerenciarMudancas} />
-          </div>
-          <div>
-            <label htmlFor="cpf">CPF</label>
-            <input type="text" name="cpf" id="cpf" value={this.state.cpf} onChange={this.gerenciarMudancas} />
-          </div>
-
-          <button type="submit">Enviar</button>
-        </form>
-      </div>
-    )
-  }
-}
-
-export default Alterar
+export default FormComponent;
--- src/graphql/clientes.js
+++ src/graphql/clientes.js
@@ -10,9 +10,29 @@ export const LISTA_CLIENTES = gql`
   }
 `;
 
+export const CONSULTA_CLIENTE = gql`
+  query Cliente($id: ID!) {
+    cliente(id: $id) {
+      id
+      nome
+      cpf
+    }
+  }
+`;
+
 export const ADICIONA_CLIENTE = gql`
   mutation adicionaCliente($nome: String!, $cpf: String!) {
     adicionaCliente(nome: $nome, cpf: $cpf) {
+      id
+      nome
+      cpf
+    }
+  }
+`;
+
+export const ATUALIZA_CLIENTE = gql`
+  mutation atualizaCliente($id: ID!, $nome: String!, $cpf: String!) {
+    atualizaCliente(id: $id, nome: $nome, cpf: $cpf) {
       id
       nome
       cpf
